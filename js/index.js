be3a662ae19b889cbdbd1a1bac3e682bbf1a8399
Renan Alfarth <renan.alfarth@capitanigroup.com.br>
2017-07-28 16:23:52 -0300

    Defining routes

--- js/index.js
+++ js/index.js
@@ -1,6 +1,20 @@
-var app = new Vue({
-  el: '#app',
-  data: {
-    message: 'Hello Vue!'
-  }
-})
+// COMPONENTS
+const Home      = { template: '<div>Home</div>' };
+const Promocoes = { template: '<div>Promoções</div>' };
+const Cotacoes  = { template: '<div>Cotações</div>' };
+const Tempo     = { template: '<div>Tempo</div>' };
+
+// ROUTES
+const routes = [
+  { path: '/', component: Home },
+  { path: '/promocoes', component: Promocoes },
+  { path: '/cotacoes', component: Cotacoes },
+  { path: '/tempo', component: Tempo }
+];
+
+// ROUTER
+const router = new VueRouter({
+  routes // short for `routes: routes`
+});
+
+var app = new Vue({router}).$mount('#app');
