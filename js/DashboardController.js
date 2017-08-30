70e726a57bcbf98a8ecd6aa1af8522ab1297105a
Renan Alfarth <renan.alfarth@capitanigroup.com.br>
2017-08-30 17:38:22 -0300

    Adding js

--- js/DashboardController.js
+++ js/DashboardController.js
@@ -1,6 +1,20 @@
 angular.module('dashboard', [])
-  .controller('DashboardController', function() {
+  .controller('DashboardController', function($http) {
 
-    
+    console.log('teste');
+
+    var req = {
+     method: 'GET',
+     url: 'https://deals-crawler.herokuapp.com/hardmob',
+     headers: {
+       'Content-Type': 'application/json'
+     }
+    }
+
+    $http(req)
+    .then(function(response){
+      console.log(response);
+    });
+
 
   });
